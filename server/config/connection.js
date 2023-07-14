a3e10ceb0e833467e4507ae535d9df1dd1462ed1
ramasantayana <rama.santayana@uconn.edu>
2023-07-14 14:38:02 -0400

    using mongodb cloud

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://stayfitwithrama:test1234@cluster0.umvato3.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://stayfitwithrama:test1234@cluster0.umvato3.mongodb.net/?retryWrites=true&w=majority');
 
 module.exports = mongoose.connection;
