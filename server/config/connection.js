0efaf7788b9151db9a3883836f593eb4e40885ee
ramasantayana <rama.santayana@uconn.edu>
2023-07-14 14:30:33 -0400

    prod conf

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cubcare');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://stayfitwithrama:test1234@cluster0.umvato3.mongodb.net/?retryWrites=true&w=majority');
 
 module.exports = mongoose.connection;
