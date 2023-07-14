24dfd1fcd8fe77224ad58383e91e4ebd4e9125e5
ramasantayana <rama.santayana@uconn.edu>
2023-07-14 15:23:48 -0400

    deleted build file

--- server/server.js
+++ server/server.js
@@ -20,10 +20,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(express.static('build'))
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
